c7b519a1c54a5f598113622afcbd88fa44e0e7e2
Molnár István <1995molnaristvan22@gmail.com>
2022-07-12 01:38:34 +0200

    Update engine.js

--- engine.js
+++ engine.js
@@ -2,3 +2,16 @@ let x = () => {
   document.querySelector('xmp').innerText = document.querySelector('#hamburger').outerHTML;
 }
 x();
+
+const genF = (elem) => {
+  return document.createElement(elem);
+};
+
+let connect = () => {
+  if (location.href.includes('github')) {
+    let baseUrl = genE('BASE');
+    document.head.insertBefore(baseUrl, null);
+    baseUrl.href = 'https://molnaristvan95.github.io/Lesson-12---Flexbox-Maket/';
+  }
+}
+connect();
